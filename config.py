fafb8e7ce9135b728d360d95eb3cb780bb9b4fdc
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2024-11-04 00:19:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ load_dotenv()
 API_ID = int(getenv("API_ID", 24740695))
 API_HASH = getenv("API_HASH", 'a95990848f2b93b8131a4a7491d97092')
 
-BOT_TOKEN = getenv("BOT_TOKEN", '7102300582:AAHBGge4ayXMEvL9gyIZh8jijZjd1bIZbkU')
+BOT_TOKEN = getenv("BOT_TOKEN", '8197260139:AAF3RWmRhUYo4GgYRqERMT_s-9Qc_3ApuiM')
 MONGO_DB_URI = getenv("MONGO_DB_URI", 'mongodb+srv://Mrdaxx123:Mrdaxx123@cluster0.q1da65h.mongodb.net/?retryWrites=true&w=majority')
 
-OWNER_ID = int(getenv("OWNER_ID", '6971100005'))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", 'https://t.me/+M7pflrUbnJhiNDZl')
+OWNER_ID = int(getenv("OWNER_ID", '7759282826'))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", 'https://t.me/ABOUT_VIP_STRANGER')
